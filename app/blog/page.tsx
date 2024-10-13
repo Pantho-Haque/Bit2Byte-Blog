c29f82484993a31a55b796705851947318b65be0
Pantho Haque <panthohaque927908@gmail.com>
2024-10-14 02:46:33 +0600

    routes and contents

---
+++ app/blog/page.tsx
@@ -0,0 +1,73 @@
+// import SampleBlogs from "@/config/sampleblogs";
+import React from "react";
+import { Button, buttonVariants } from "@/components/ui/button";
+import fs, { readFileSync } from "fs";
+import matter from "gray-matter";
+import Link from "next/link";
+import { Metadata } from "next";
+
+interface BlogType {
+  slug: string;
+  title: string;
+  description: string;
+  imageUrl?: string;
+}
+
+const dirContent = fs.readdirSync("content", "utf-8");
+console.log(dirContent);
+
+let blogs: BlogType[] = dirContent.map((file) => {
+  const fileContent = readFileSync(`content/${file}`, "utf-8");
+  const { data } = matter(fileContent);
+  const value: BlogType = {
+    slug: data.slug,
+    title: data.title,
+    description: data.description,
+    imageUrl: data?.imageUrl,
+  };
+  return value;
+});
+
+console.log(blogs);
+// blogs = [];
+
+const BlogList = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6 text-center my-2">Our Blogs</h1>
+      {blogs.length == 0 && <p className="text-7xl font-semibold mt-10 w-full text-center text-gray-600">No Blogs found</p>}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-4/5 mx-auto">
+        {blogs.map((blog: BlogType, index: number) => (
+          <div
+            key={index}
+            className="shadow-lg rounded-lg overflow-hidden border-2 pb-3 flex flex-col justify-between"
+          >
+            <div className="p-4 ">
+              <img
+                className="w-full h-64 object-cover object-top"
+                src={blog.imageUrl ? blog.imageUrl : "/blogimg.jpg"}
+                alt={blog.title}
+              />
+              <h2 className="text-xl font-semibold mb-2">{blog.title}</h2>
+              <p className="mb-4">{blog.description}</p>
+            </div>
+            <Link
+              href={`/blogpost/${blog.slug}`}
+              className={`${buttonVariants({ variant: "default" })} w-1/3 ml-4`}
+            >
+              Read More
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const metadata: Metadata = {
+  title: "Blogs - Bit2Byte",
+  description:
+    "A comprehensive blog for coders of all levels, from beginners to advanced. Explore tutorials, tips, and insights on a wide range of programming languages and technologies. Stay up-to-date with the latest trends in software development, learn best practices, and enhance your coding skills with in-depth articles and guides.",
+};
+
+export default BlogList;
---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section className="text-gray-600 body-font relative">
+      contact
+    </section>
+  );
+};
+
+export default Contact;
