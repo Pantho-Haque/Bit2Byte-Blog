f70d7d7ad2e8a6b36562064717b54d594fc254f9
iqbal <iqbal.official.ac@gmail.com>
2024-11-20 01:24:37 +0600

    responsive design

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion"; // Import framer-motion
+import { motion, AnimatePresence } from "framer-motion";
 import Banner from "@/components/login/banner";
 import TabButtonGroup from "@/components/login/tab-button-group";
 
@@ -10,29 +10,34 @@ import RegisterForm from "@/components/login/signup-form";
 
 const Login = () => {
   const [isSignUp, setIsSignUp] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
 
   const handleToggle = (signUp: boolean) => {
     setIsSignUp(signUp);
   };
 
   return (
-    <div className="flex min-h-screen">
-      <Banner />
-      <div className="flex-1 flex flex-col justify-center items-center p-8 bg-white dark:bg-slate-700 shadow-lg">
+    <div className="flex flex-col lg:flex-row min-h-screen">
+      {/* Banner */}
+      <div className="lg:w-1/2 bg-indigo-600 hidden lg:flex items-center justify-center">
+        <Banner />
+      </div>
+
+      {/* Main Content */}
+      <div className="w-full lg:w-1/2 flex flex-col justify-center items-center p-6 lg:p-8 bg-white dark:bg-slate-700 shadow-lg">
+        {/* Tab Button Group */}
         <TabButtonGroup isSignUp={isSignUp} handleToggle={handleToggle} />
+
+        {/* Animated Form Container */}
         <motion.div
-          className="max-w-md w-full p-8 rounded-lg border-t-4 border-indigo-600 shadow-lg flex flex-col items-start"
+          className="w-full max-w-md p-6 lg:p-8 rounded-lg border-t-4 border-indigo-600 shadow-lg flex flex-col items-start"
           layout
           transition={{ type: "spring", stiffness: 500, damping: 30 }}
         >
-          <h2 className="text-3xl font-bold mb-6 text-center text-gray-800 dark:text-white w-full">
+          <h2 className="text-2xl lg:text-3xl font-bold mb-6 text-center text-gray-800 dark:text-white w-full">
             {isSignUp ? "Sign Up" : "Sign In"}
           </h2>
+
+          {/* Animate Presence for Form Switching */}
           <AnimatePresence mode="wait">
             {isSignUp ? (
               <RegisterForm key="signUp" />
